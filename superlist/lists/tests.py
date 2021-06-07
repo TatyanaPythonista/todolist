5f800db334fa5b9eedc58667e6024a010b133acd
Tatyana Egorenkova <[tatyana.egorenkova.19@gmail.com]>
2021-06-07 09:20:36 +0700

    pribe

--- superlist/lists/tests.py
+++ superlist/lists/tests.py
@@ -11,3 +11,5 @@ class HomePageTest(TestCase):
         """Тест: используется домашний шаблон"""
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'lists/home.html')
+
+
