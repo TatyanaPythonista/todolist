36811327b8bb18241d9d564dc68256020cf16d6f
Tatyana Egorenkova <[tatyana.egorenkova.19@gmail.com]>
2021-06-10 22:32:23 +0700

    Use LiveServerTestCase

--- superlist/functional_tests/tests.py
+++ superlist/functional_tests/tests.py
@@ -1,10 +1,11 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     """Тест нового посетителя"""
 
     def setUp(self):
@@ -25,7 +26,7 @@ class NewVisitorTest(unittest.TestCase):
         """Тест: Можно начать список и получить его позже"""
         # Эдит слышала про крутое новое онлайн-приложение со списком неотложных дел.
         # Она решает оценить его домашнюю страницу
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # Она видит, что заголовок и шапка страницы говорят о стисках неотложных дел
         self.assertIn('To-Do', self.browser.title)
@@ -67,7 +68,3 @@ class NewVisitorTest(unittest.TestCase):
 # Она посещает этот url-адрес - её список по-прежнему там.
 
 # Удоблетворенная, она снова ложится спать.
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
