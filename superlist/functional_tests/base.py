be78145b8ceb6f09c50c7705c5715688a9b577f9
Tatyana Egorenkova <[tatyana.egorenkova.19@gmail.com]>
2021-06-30 12:23:21 +0700

    Add functional tests

--- superlist/functional_tests/base.py
+++ superlist/functional_tests/base.py
@@ -35,3 +35,14 @@ class FunctionalTest(StaticLiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise ex
                 time.sleep(0.5)
+
+    def wait_for(self, fn):
+        """Ожидать"""
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as ex:
+                if time.time() - start_time > MAX_WAIT:
+                    raise ex
+                time.sleep(0.5)
