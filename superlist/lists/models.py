a82aad409dc6fec627209926075b74bb0a793245
Tatyana Egorenkova <[tatyana.egorenkova.19@gmail.com]>
2021-07-03 14:00:24 +0700

    Duplicate items are invalid

--- superlist/lists/models.py
+++ superlist/lists/models.py
@@ -7,6 +7,14 @@ class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey('List', default=None, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ('id', )
+        unique_together = ('list', 'text')
+
+    def __str__(self):
+        return self.text
+
+
 
 class List(models.Model):
     """Список"""
